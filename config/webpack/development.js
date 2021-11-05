52c00471342adc6619020dd7006bfb4878d522eb
RozaHar <rozaharutyuny.129@gmail.com>
2021-11-05 10:39:35 -0700

    Added bootstrap

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
+const webpack = require('webpack')
+ environment.plugins.append
+ ('Provide', new webpack.ProvidePlugin(
+ 	{ $: 'jquery', jQuery: 'jquery', 
+ 	Popper: ['popper.js', 'default'] 
+ }) 
+) 
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
